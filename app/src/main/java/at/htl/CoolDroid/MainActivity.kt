096d63a4e2f169be83a84f659f9162ed79197ef3
SimonKoll <simonkoll20@gmail.com>
2021-10-18 11:25:57 +0200

    should work

--- app/src/main/java/at/htl/CoolDroid/MainActivity.kt
+++ app/src/main/java/at/htl/CoolDroid/MainActivity.kt
@@ -2,17 +2,29 @@ package at.htl.leonding.cooldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import at.htl.CoolDroid.R
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    internal val LOG_TAG = MainActivity::class.java.simpleName
+    var counter = 0
     // old way:
     //var tvMessage: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvCoolDroid.text="New way"
+        ivCoolDroid.setOnClickListener{
+            counter++
+            Log.d(LOG_TAG, "onCreate(), counter: ${counter}")
+            tvCoolDroid.text =
+                when (counter) {
+                    1 -> getString(R.string.txtOnce)
+                    2 -> getString(R.string.txtTwice)
+                    else -> getString(R.string.textViewNumbers, counter)
+                }
+        }
+
 
         //old way:
         //tvMessage = findViewById(R.id.tvCoolDroid)
